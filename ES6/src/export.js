b8d8bda6552d28b072617cc1db6554698ac6e6ee
Magnet <1057347911@qq.com>
2018-08-03 15:16:38 +0800

    1

--- ES6/src/export.js
+++ ES6/src/export.js
@@ -1,7 +1,25 @@
 // export let a = 10;
 // export let foo=(a,b)=>a+b;
 
+// export let a=10;
+// export let foo=(a,b)=> a+b;
+// let obj = {
+//   name:"xiaoming"
+// }
+// export default obj;
 
+// class People{
+//   constructor(name, age){
+//       this.name = name;
+//       this.age = age;
+//   }
+//   showName(){
+//       console.log('我叫' + this.name);
+//   }
+// }
+let bb= 10;
+
+export default bb;
 
 
 export default a;
--- ES6/src/import.js
+++ ES6/src/import.js
@@ -1,7 +1,17 @@
 // import {a,foo} from './export.js'  //./在当前文件夹下
 /*import {a,foo} from './export.js'*/
 
-import  bb from './export'
+//import  bb from './export'
 
 console.log(bb);
 //console.log(foo(3,2));
+
+// import {aaaa} from './export.js'
+// console.log(aaaa);
+// // console.log(foo(3,5));
+
+
+import bb from './export.js';
+console.log(bb);
+// let p1 = new bbb('zs', 18);
+// p1.showName();
