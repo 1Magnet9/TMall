88870fc50db6bd9edb62364461b61ff2f8b7f6b5
Magnet <1057347911@qq.com>
2018-08-03 15:10:26 +0800

    1

---
+++ ES6/src/export.js
@@ -0,0 +1,10 @@
+// export let a = 10;
+// export let foo=(a,b)=>a+b;
+
+
+
+
+export default a;
+
+
+let a = 111;
---
+++ ES6/src/import.js
@@ -0,0 +1,7 @@
+// import {a,foo} from './export.js'  //./在当前文件夹下
+/*import {a,foo} from './export.js'*/
+
+import  bb from './export'
+
+console.log(bb);
+//console.log(foo(3,2));
