2489d1201ca089b68a31a07949e01d6df9145804
Magnet <1057347911@qq.com>
2018-08-11 11:28:35 +0800

    1

---
+++ cli/vuecli/src/views/Connect.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>contact us!{{$store.state.count}}</h1>
+        <router-link :to="{name:'test1',params:{name:'lisi',age:'30'}}">test1</router-link> |
+        <!--<router-link :to="{name:'test1',params:{name:'lisi',age:'30'}}">test1</router-link>-->
+        <router-link to="/connect/test2/zhangsan/10">test2</router-link> |
+        <router-link to="/connect/backtest2/zhangsan/10">back test2</router-link>
+        <div>
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
